2a76b8a1dfafe0db88faba29a1b4b1d990a41178
Rikio Campbell <rikiocamp@gmail.com>
2021-01-26 15:04:02 -0800

    connected save buttons to corresponding text fields

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 var hours = 24
 // variable to add hour id's to form fields
 var stringTime = ["12 AM","1 AM","2 AM","3 AM","4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","11 PM"]
+// variable to add id's to save buttons
+// var btnNum = ["0000","0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300","1400","1500","1600","1700","1800","1900","2000","2100","2200","2300"]
+var btnVal = 0
 
 // create 1 row for each hour for a total of 24 rows
 for (i=0;i<hours;i++){
@@ -33,7 +36,7 @@ for (i=0;i<hours;i++){
     // add class and style to text area form
     textArea.addClass("form-control")
     textArea.attr("style", "background-color: #90EE90;")
-    // add class/id to save button
+    // add class/generic id to save button
     saveButton.addClass("btn btn-primary")
     saveButton.attr("id", "saveButton")
     // append row to main div
@@ -50,11 +53,28 @@ for (i=0;i<hours;i++){
     // set hour text in hour column
     $("#timeText").text(stringTime[i])
     $("#saveButton").text("Save")
-    // remove id attributes for items for loop to continue working (id's are not necessary moving forward for these items)
+    // remove generic id attributes for items for loop to continue working
     $("#timeText").removeAttr("id")
     $("#hourCol").removeAttr("id")
     $("#timeRow").removeAttr("id")
     $("#infoCol").removeAttr("id")
     $("#saveCol").removeAttr("id")
     $("#saveButton").removeAttr("id")
+    // add unique ids/values to textArea and saveButton
+    textArea.attr("id", stringTime[i])
+    saveButton.attr("value", btnVal)
+    // increase btnVal value for next save button
+    btnVal++
 }
+
+$(".btn-primary").on("click", function() {
+    // create variable equal to button value. 
+    var matchVal = $(this).attr("value")
+    // create variable equal to stringTime array index item of matchVal. This turns matchVal into the full time.
+    var stringVal = stringTime[matchVal]
+    
+    // console.log($("textarea").eq(matchVal).val())
+    // set local storage to the item in the corresponding text area for the button
+    localStorage.setItem(stringVal,JSON.stringify($( "textarea" ).eq(matchVal).val()));
+
+  });
